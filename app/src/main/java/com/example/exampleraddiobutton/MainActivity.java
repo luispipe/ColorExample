70e088eeb880116ae313d411f7b19b304800596e
luispipe <lflondonor@unal.edu.co>
2023-08-10 20:24:13 -0300

    color final commit

--- app/src/main/java/com/example/exampleraddiobutton/MainActivity.java
+++ app/src/main/java/com/example/exampleraddiobutton/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.exampleraddiobutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +28,16 @@ public class MainActivity extends AppCompatActivity {
         RadioButton boton2= findViewById(R.id.radioButtonRed);
         Button button= findViewById(R.id.button);
 
+        LinearLayout circle2= findViewById(R.id.layoutCircle);
+
+
         boton1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View v) {
                 boton2.setChecked(false);
                 button.setBackgroundColor(getColor(R.color.blue));
+                circle2.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
             }
         });
 
@@ -31,9 +46,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 boton1.setChecked(false);
                 button.setBackgroundColor(getColor(R.color.red));
+                circle2.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF1100")));
             }
         });
 
 
     }
+
 }
